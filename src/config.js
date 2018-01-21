2faf56bc2471b64bdf80e2a4d5fca113e08b795e
juhahinkula <hinkula@gmail.com>
2018-01-21 17:18:28 +0200

    refactoring

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Change you firebase settings here 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeds8KY-Jw6gbcxgzhw9rRkmEc3Bm2ISc",
-  authDomain: "reactauthentication-8ba67.firebaseapp.com",
-  databaseURL: "https://reactauthentication-8ba67.firebaseio.com",
-  projectId: "reactauthentication-8ba67",
-  storageBucket: "reactauthentication-8ba67.appspot.com",
-  messagingSenderId: "315993440137"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
